19b989741ca6b0e86b3965b90a3ad202c940a484
Wang Xu <gnawux@gmail.com>
2015-04-14 15:32:50 +0800

    add unit test for qemu context :older_man:
    Signed-off-by: Wang Xu <gnawux@gmail.com>

---
+++ api/qemu/context_test.go
@@ -0,0 +1,41 @@
+package qemu
+
+import (
+    "testing"
+)
+
+func TestInitContext(t *testing.T) {
+
+    ctx := initContext("vmid", nil, 1, 128)
+    if ctx.id != "vmid" {
+        t.Error("id should be vmid, but is ", ctx.id)
+    }
+
+}
+
+func testJson(key string) string {
+    jsons := make(map[string]string)
+
+    jsons["basic"] =`{
+    "name": "hostname",
+    "containers" : [{
+        "image": "nginx:latest",
+        "files":  [{
+            "path": "/var/lib/xxx/xxxx",
+            "filename": "filename"
+        }]
+    }],
+    "resource": {
+        "vcpu": 1,
+        "memory": "128"
+    },
+    "files": [{
+        "name": "filename",
+        "encoding": "raw",
+        "uri": "https://s3.amazonaws/bucket/file.conf",
+        "content": ""
+    }],
+    "volumes": []}`
+
+    return jsons[key]
+}
