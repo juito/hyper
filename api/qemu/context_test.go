bbebf92b78b29fb2ffc1c0cd21a32eba79fc924e
Wang Xu <gnawux@gmail.com>
2015-04-14 23:23:32 +0800

    qemu context tests
    Signed-off-by: Wang Xu <gnawux@gmail.com>

--- api/qemu/context_test.go
+++ api/qemu/context_test.go
@@ -2,14 +2,24 @@ package qemu
 
 import (
     "testing"
+    "encoding/json"
+    "dvm/api/pod"
 )
 
 func TestInitContext(t *testing.T) {
 
-    ctx := initContext("vmid", nil, 1, 128)
+    ctx := initContext("vmid", nil, 3, 202)
+
     if ctx.id != "vmid" {
         t.Error("id should be vmid, but is ", ctx.id)
     }
+    if ctx.cpu != 3 {
+        t.Error("cpu should be 3, but is ", string(ctx.cpu))
+    }
+    if ctx.memory != 202 {
+        t.Error("memory should be 202, but is ", string(ctx.memory))
+    }
+
     t.Log("id check finished.")
     ctx.Close()
 }
@@ -18,6 +28,32 @@ func TestRemoveSock(t *testing.T) {
     initContext("vmid", nil, 1, 128)
     initContext("vmid", nil, 1, 128)
     t.Log("repeat initiated them.")
+}
+
+func TestParseSpec(t *testing.T) {
+    ctx := initContext("vmmid", nil, 1, 128)
+
+    spec := pod.UserPod{}
+    err := json.Unmarshal([]byte(testJson("basic")), &spec)
+    if err != nil {
+        t.Error("parse json failed ", err.Error())
+    }
+
+    ctx.InitDeviceContext(&spec, 0)
+
+    if ctx.userSpec != &spec {
+        t.Error("user pod assignment fail")
+    }
+
+    if len(ctx.vmSpec.Containers) != 1 {
+        t.Error("wrong containers in vm spec")
+    }
+
+    res,err := json.MarshalIndent(*ctx.vmSpec, "    ", "    ")
+    if err != nil {
+        t.Error("vmspec to json failed")
+    }
+    t.Log(string(res))
 }
 
 func testJson(key string) string {
@@ -34,7 +70,7 @@ func testJson(key string) string {
     }],
     "resource": {
         "vcpu": 1,
-        "memory": "128"
+        "memory": 128
     },
     "files": [{
         "name": "filename",
