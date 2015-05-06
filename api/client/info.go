5d6bb8569f1eaeef1f1d63da23957cfe10b7dd30
Lei Xue <carmark.dlut@gmail.com>
2015-05-06 16:07:37 +0800

    modify the 'info' command help message

--- api/client/info.go
+++ api/client/info.go
@@ -3,10 +3,17 @@ package client
 import (
 	"fmt"
 	"dvm/engine"
+	gflag "github.com/jessevdk/go-flags"
 )
 
 // we need this *info* function to get the whole status from the docker daemon
 func (cli *DvmClient) DvmCmdInfo(args ...string) error {
+	var parser = gflag.NewParser(nil, gflag.Default)
+	parser.Usage = "info\n\nDisplay system-wide information"
+	args, err := parser.Parse()
+	if err != nil {
+		return nil
+	}
 	body, _, err := readBody(cli.call("GET", "/info", nil, nil))
 	if err != nil {
 		fmt.Printf("The Error is encountered, %s\n", err)
